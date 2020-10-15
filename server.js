1ee118148879a3646d1a581b12f706de7fcc1f0b
jacob kleiman <jacobakleiman@gmail.com>
2020-10-14 17:19:28 -0700

    adds logger

--- server.js
+++ server.js
@@ -1,34 +1,49 @@
 // import requireed node modules into variables
-require("dotenv").config()
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const passport = require("passport")
-const users = require("./routes/api/users")
-const port = process.env.PORT || 8000
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const passport = require('passport');
+const users = require('./routes/api/users');
+const port = process.env.PORT || 8000;
 
 // middleware
-app.use(cors())
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Custom Request Logger Middleware
+app.use((req, res, next) => {
+  const url = req.url;
+  const method = req.method;
+
+  // Destructuring
+  // const { url, method } = req;
+
+  const requestedAt = new Date().toLocaleTimeString();
+  const result = `${method} ${url} ${requestedAt}`;
+  console.log(result);
+
+  next();
+});
 
 // passport middleware
-app.use(passport.initialize())
+app.use(passport.initialize());
 
 // importing passport file into server
-require("./config/passport")(passport)
+require('./config/passport')(passport);
 
 // <<<<<<<<  SERVER HOME ROUTE >>>>>>>>>>>
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res
     .status(200)
-    .json({ message: "Smile, You are being watched by the Backend Team" })
-})
+    .json({ message: 'Smile, You are being watched by the Backend Team' });
+});
 
 // api/users route middleware
-app.use("/api/users", users)
+app.use('/api/users', users);
 
 // SERVER PORT TO LISTEN
 app.listen(port, () => {
-  console.log(`You are lisening smooth to port ${port}`)
-})
+  console.log(`You are lisening smooth to port ${port}`);
+});
